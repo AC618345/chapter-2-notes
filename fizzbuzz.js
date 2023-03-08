e1a8c3d857f0f347d3bfb1f2668abaf24fe5e2e7
ywkh <ywkh@fresnou.org>
2023-03-08 10:02:39 -0800

    initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+let number = 0;
+while (number<100) {
+
+}
+    number++;
+    if (number % 3 == 0){
+        console.log('Fizz');
+    }
+    else if (number % 5 == 0) {
+    }
+
+        console.log('Buzz');}
+    else if (number % 15 == 0){
+    console.log('FizzBuzz');
+}
---
+++ practicequiz.js
@@ -0,0 +1,38 @@
+//let secret = 15;
+//secret --;
+//console.log(secret);
+
+//let myNum = 30;
+//myNum ++;
+//console.log(myNum);
+
+// let value = 20;
+// value = value % 3;
+// value += 10;
+// value -= 2;
+// console.log(value)
+
+// let age = 27;
+// if(age >= 65){
+//     console.log('retirement age');
+// } else if (age > 18){
+//     console.log('adult');
+// } else {
+//     console.log('not eligible');
+// }
+//}
+
+// let z = 60;
+// while (z > 50){
+//     z -= 2;
+//
+//     console.log(z);
+// }
+
+// for (let i=0; i<3; i++){
+//     console.log(i);
+// }
+
+for(let i = 10; i>=2; i--){
+    console.log('Hello!')
+}
